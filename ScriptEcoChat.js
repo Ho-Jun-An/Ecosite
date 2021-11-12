2e934f3b9dc8503eac2d5f2ea645a9fbf254aea3
Weizhong Lee <s10194072@connect.np.edu.sg>
2021-11-12 12:32:57 +0800

    I added the necessary HTML boilerplate as Jun An didn't think to after assuming that HTML files from ASP.NET MVC projects can be used straightaway without adding the boilerplate to them. Also I need to test and pray that Visual Studio Code can push whatever I changed to his repository.

---
+++ ScriptEcoChat.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const inputField = document.getElementById("input")
+    inputField.addEventListener("keydown", function(e) {
+        if (e.code === "Enter") {
+            let input = inputField.value;
+            inputField.value = "";
+            output(input);
+    }
+  });
+});
