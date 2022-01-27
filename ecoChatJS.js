4cc6b3a6bd283eb40fda09c43cada41feca43e4f
Ho Jun An <Jun-An@Live.com>
2022-01-27 20:26:46 +0800

    Minor changes to ecoChat

--- ecoChatJS.js
+++ ecoChatJS.js
@@ -10,7 +10,8 @@
 document.addEventListener("DOMContentLoaded", () => 
 {
   // Select ecoChat textbox
-  const ecoChatInputbox = document.getElementById("ecoChat-textbox");
+  const ecoChatTextbox = document.getElementById("ecoChat-textbox");
+  const ecoChatInputbox = document.getElementById("ecoChat-inputbox");
 
   // Create bot icon
   let botIcon = document.createElement("img");
@@ -18,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () =>
   botIcon.className = "botIcon"
   botIcon.src = "images/ecoChat-icon.png"
   // Add bot icon to html
-  ecoChatInputbox.appendChild(botIcon);
+  ecoChatTextbox.appendChild(botIcon);
 
   // Create div for bot reply
   let botReply = document.createElement("div");
@@ -27,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () =>
   // Add reply for div
   botReply.innerHTML = "Hi! Welcome to EcoChat. Ask me about unsustainable/ sustainable, (non-)environmentally friendly brands or type in /help.";
   // Add reply to html
-  ecoChatInputbox.appendChild(botReply);
+  ecoChatTextbox.appendChild(botReply);
 
   // Detect user pressing Enter key.
   ecoChatInputbox.addEventListener("keydown", function(e) 
@@ -36,6 +37,27 @@ document.addEventListener("DOMContentLoaded", () =>
     {
       // Get text from <input> in ecoChat.html
       let input = document.getElementById("ecoChat-inputbox").value;
+
+      // Select ecoChat textbox
+      const ecoChatTextbox = document.getElementById("ecoChat-textbox");
+
+      // Create user icon
+      let userIcon = document.createElement("img");
+      // Assign class and img for user icon
+      userIcon.className = "userIcon"
+      userIcon.src = "images/ecoChat-icon.png"
+      // Add user icon to html
+      ecoChatTextbox.appendChild(userIcon);
+
+      // Create div for user reply
+      let userReply = document.createElement("div");
+      // Assign class for user reply div
+      userReply.className = "userReply";
+      // Add reply for div
+      userReply.innerHTML = `${input}`;
+      // Add reply to html
+      ecoChatTextbox.appendChild(userReply);
+
       output(input); 
     }
   });
@@ -67,25 +89,36 @@ function output(input)
   }
 
   //clear user input
-  document.getElementById("input").value = "";
+  document.getElementById("ecoChat-inputbox").value = "";
 
   // Updates DOM.
-  addChat(input, product);
+  addChat(product);
 }
 
-function addChat(input, product) 
+function addChat(product) 
 {
-  const maiecoChatInputboxnDiv = document.getElementById("ecoChat-textbox");
-  let userDiv = document.createElement("div");
-  userDiv.id = "user";
-  userDiv.innerHTML = `<table> <tr> <td class = "top"> You: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </td> <td> <span id="bot-response">${input}</span> </td> </tr> </table>`;
-  ecoChatInputbox.appendChild(userDiv);
+  // Select ecoChat textbox
+  const ecoChatTextbox = document.getElementById("ecoChat-textbox");
 
+  // Create bot icon
+  let botIcon = document.createElement("img");
+  // Assign class and img for bot icon
+  botIcon.className = "botIcon"
+  botIcon.src = "images/ecoChat-icon.png"
+  // Add bot icon to html
+  ecoChatTextbox.appendChild(botIcon);
+
+  // Create div for bot reply
   let botReply = document.createElement("div");
-  botReply.id = "bot";
-  botReply.innerHTML = `<table> <tr> <td class = "top"> Chatbot: &nbsp; &nbsp; </td> <td> <span id="bot-response">${product}</span> </td> </tr> </table>`;
-  ecoChatInputbox.appendChild(botReply);
-  //speak(product);
+  // Assign class for bot reply div
+  botReply.className = "botReply";
+  // Add reply for div
+  botReply.innerHTML = `${product}`;
+  // Add reply to html
+  ecoChatTextbox.appendChild(botReply);
+
+  // Scroll to bottom
+  ecoChatTextbox.scrollTop = ecoChatTextbox.scrollHeight - ecoChatTextbox.clientHeight;
 }
 
 // If the word or phrase is in the double quotes, 
@@ -122,7 +155,7 @@ const reply =
   ["Hello, may I help you?"],
 
   //1
-  ["Here are some unsustainable brands:" + 
+  ["Here are some unsustainable brands: " + 
   "<ul>" +
     "<li>" +
       "Shein" +
@@ -139,7 +172,7 @@ const reply =
   //2
   ["Shein: <br> Although Shein has a social responsibility website to show the company\'s commitments to giving back to society. There have been no forms of third-party certification to verify that Shein has been genuinely contributing to becoming more sustainable environmentally and in other aspects. <br> Shein has been shown to sell their clothes at relatively lower prices compared to other fashion companies and has a fast turnaround time due to their transportation of their goods via airplanes which consumes large amount of fossil fuels, thus having a larger carbon footprint. In addition, there is no evidence to show that Shein pays their factory workers a proper wage which allows the company to operate with low overhead costs. This allows for the company to sell their products at such low prices.<br> <a href = 'https://www.greenmatters.com/p/is-shein-bad'> Find out more here. </a>", 
   "Victoria\'s Secret: <br> Victoria\'s Secret has little eco-friendly materials in its collection. It has also signed up with Greeenpeace\'s “Detox My Fashion” program back in 2011 which set a deadline to eliminate hazardous chemicals by 2020 from their products. <br> However, the company has not produced any evidence of their progress towards their set goal. The brand has received 21%-30% in the Fashion Transparency Index, thus making them less trustworthy. With the onset of the COVID-19 pandemic, Victoria\'s Secret has shown the lack of policies in protecting suppliers and workers in it supply chain from the impacts of the pandemic. <br> <a href = 'https://goodonyou.eco/how-ethical-is-victorias-secret/'> Find out more here. </a>",
-  "Mango: <br> The company although disclosing the number of its factories\' greenhouse gas emissions, it has yet to set a target to lower them and have yet to have a plan on how to cut down on such emissions. Mango\'s Code of Conduct has also shown that it only pays their workers the legal minimum wage and not the recommended wages. <br> <a href = 'https://www.independent.co.uk/news/uk/home-news/which-british-high-street-shops-do-not-pay-their-garment-workers-living-wage-9223733.html'> Find out more here. </a> <br> <a href = 'https://www.sustainably-chic.com/blog/fast-fashion-brands-to-avoid'> and here. </a>"],
+  "Mango: <br> The company although disclosing the number of its factories\' greenhouse gas emissions, it has yet to set a target to lower them and have yet to have a plan on how to cut down on such emissions. Mango\'s Code of Conduct has also shown that it only pays their workers the legal minimum wage and not the recommended wages. <br> <a href='https://www.independent.co.uk/news/uk/home-news/which-british-high-street-shops-do-not-pay-their-garment-workers-living-wage-9223733.html'>Find out more here</a> <a href='https://www.sustainably-chic.com/blog/fast-fashion-brands-to-avoid'>and here.</a>"],
 
   //3
   ["Here are some sustainable brands:" + 
@@ -157,9 +190,9 @@ const reply =
   "<br> Pick one to know why."],
 
   //4
-  ["Levis: <br> Known for their denim wear, the company has introduced Water < Less which uses up to 96% less water in the creation of the material which is known for its huge requirements in terms of water usage. In addition, Levi\'s has pledged to gear their entire design and manufacturing process towards sustainability via the usage of 100% sustainably sourced cotton and recycling old jeans into home insulation. <br> <a href = 'https://www.forbes.com/sites/blakemorgan/2020/02/24/11-fashion-companies-leading-the-way-in-sustainability/?sh=3bb630916dba'> Find out more here. </a>", 
-  "Uniqlo: <br> Uniqlo has set targets to reduce emission of harmful gasses in its supply chain, however it does not publicly declare its progress in such goals. In terms of labour conditions, it was given the 31-40% on the Fashion Transparency Index. Uniqlo has also failed to provide evidence that it ensures their workers are paid a living wage. <br> <a href = 'https://goodonyou.eco/how-ethical-is-uniqlo/'> Find out more here. </a>",
-  "Zalora: <br> Zalora has shown their commitment to further their sustainability through introducing concrete measures that are geared towards their People and Planet Positive Agenda. The introduction of Earth Edit, Zalora\'s sustainable shopping edit with carefully defined sustainable criteria which includes 5% of Zalora\'s retail branded portfolio. A significant reduction in the impact of its own operations, including diverting 86% of fulfilment centre waste from landfills main to recycling, and sourcing 68% of its delivery and internal packaging from sustainable materials. <br> <a href = 'https://global-fashion-group.com/2021/04/22/global-fashion-group-announces-the-release-of-its-inaugural-sustainability-report-in-southeast-asia/ '> Find out more here."],
+  ["Levis: <br> Known for their denim wear, the company has introduced Water < Less which uses up to 96% less water in the creation of the material which is known for its huge requirements in terms of water usage. In addition, Levi\'s has pledged to gear their entire design and manufacturing process towards sustainability via the usage of 100% sustainably sourced cotton and recycling old jeans into home insulation. <br> <a href='https://www.forbes.com/sites/blakemorgan/2020/02/24/11-fashion-companies-leading-the-way-in-sustainability/?sh=3bb630916dba'>Find out more here.</a>", 
+  "Uniqlo: <br> Uniqlo has set targets to reduce emission of harmful gasses in its supply chain, however it does not publicly declare its progress in such goals. In terms of labour conditions, it was given the 31-40% on the Fashion Transparency Index. Uniqlo has also failed to provide evidence that it ensures their workers are paid a living wage. <br> <a href='https://goodonyou.eco/how-ethical-is-uniqlo/'>Find out more here.</a>",
+  "Zalora: <br> Zalora has shown their commitment to further their sustainability through introducing concrete measures that are geared towards their People and Planet Positive Agenda. The introduction of Earth Edit, Zalora\'s sustainable shopping edit with carefully defined sustainable criteria which includes 5% of Zalora\'s retail branded portfolio. A significant reduction in the impact of its own operations, including diverting 86% of fulfilment centre waste from landfills main to recycling, and sourcing 68% of its delivery and internal packaging from sustainable materials. <br> <a href='https://global-fashion-group.com/2021/04/22/global-fashion-group-announces-the-release-of-its-inaugural-sustainability-report-in-southeast-asia/ '>Find out more here.</a>"],
 
   //5
   ["Perhaps you could learn more about the topic you're interested in by emailing us at <a href = 'emailto:ecositee@gmail.com'> ecositee@gmail.com"],
@@ -205,21 +238,6 @@ function compare(triggerArray, replyArray, text)
           }
         }
       }
-      
-      
-      // if (triggerArray[x][y] == text) 
-      // {
-      //   if (triggerArray[x].length == replyArray[x].length)
-      //   {
-      //     item = replyArray[x][y];
-      //     break;
-      //   }
-      //   else
-      //   {
-      //     item = replyArray[x][0];
-      //     break;
-      //   }
-      // }
     }
   }
   return item;
